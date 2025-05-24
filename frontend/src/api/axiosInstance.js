7068c5647dc8411f6e8f1053060dc92e54ed1585
ArvidionHavas <142262748+Arvidion@users.noreply.github.com>
2025-05-24 17:41:53 +0700

    Update axiosInstance.js

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -2,8 +2,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://notes-backend-wijdan-13926268988.us-central1.run.app", // Ganti dengan base URL backend-mu
-  withCredentials: true, // untuk kirim cookie kalau dibutuhkan
+  baseURL: "backend-havas-352792921038.us-central1.run.app", 
+  withCredentials: true, 
 });
 
 export default instance;
