bd9afb5605e98e809fa50298a067a0e50862431c
ArvidionHavas <142262748+Arvidion@users.noreply.github.com>
2025-05-24 17:14:07 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,29 @@
 import express from "express";
 import cors from "cors";
 import route from "./routes/route.js";
-import "./model/index.js"; // <- PENTING: memicu sinkronisasi DB
+import "./model/index.js";
 import cookieParser from "cookie-parser";
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 7000;
 
-// Konfigurasi CORS agar mengizinkan domain frontend terdeploy
 const corsOptions = {
  origin: [
-    //"https://notes-frontend-wijdan-dot-projek-tcc-1.uc.r.appspot.com",
-    "http://localhost:3000", // Tambahkan localhost untuk pengembangan lokal
-  ], // Ganti dengan URL frontend yang terdeploy
-  credentials: true, // Memungkinkan penggunaan cookies
-  methods: ["GET", "POST", "PUT", "DELETE"],
+    "https://backend-havas-352792921038.us-central1.run.app",
+    "http://localhost:3000",
+  ],
+  credentials: true,
+  methods: [
+   "GET", 
+   "POST", 
+   "PUT", 
+   "DELETE"
+  ],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
 
-app.use(cors(corsOptions)); // Menggunakan opsi CORS
-
-// Menambahkan penanganan preflight request (OPTIONS)
-app.options("*", cors(corsOptions)); // Menanggapi preflight requests
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 
 app.use(express.json());
 app.use(cookieParser());
